f5eeddf115e0782b7126e3cf06574cf5ac17ec48
gracewildingbs <gwilding@beeverstruthers.co.uk>
2025-07-23 15:59:28 +0100

    minor change to test default branch

--- pages/app.py
+++ pages/app.py
@@ -7,8 +7,6 @@ st.set_page_config(page_title="Homepage")
 
 st.write("Test")
 
-
-
 data_page = st.Page("calculate_data.py", title="Create entry")
 feedback_page = st.Page("feedback.py", title="Feedback")
 pg = st.navigation([data_page, feedback_page])
