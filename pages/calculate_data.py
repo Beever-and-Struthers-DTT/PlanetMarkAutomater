c88a0c562ea4116dbdc4b48f7d6a3d39e5c92fff
gracewildingbs <gwilding@beeverstruthers.co.uk>
2025-09-15 14:19:24 +0100

    test uploading file into pandas df

--- pages/calculate_data.py
+++ pages/calculate_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 
@@ -5,3 +6,10 @@ st.set_page_config(page_title="Practice Engine data")
 
 
 uploaded_file = st.file_uploader("Upload the Excel sheet here")
+
+if uploaded_file is not None:
+    global df
+    for sheet_name, df in pd.read_excel(r"TravelInfo.xlsx", index_col=0, sheet_name=None).items():
+        combined_df = pd.concat([df], ignore_index=True)
+
+    st.dataframe(combined_df)
