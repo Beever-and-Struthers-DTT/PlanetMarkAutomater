7e7ddcbc1822033203ee1b70b13ab7c5af41ebb6
gracewildingbs <gwilding@beeverstruthers.co.uk>
2025-09-15 14:32:11 +0100

    import library to read excel files

--- pages/app.py
+++ pages/app.py
@@ -6,6 +6,5 @@ st.title("Planet Mark Submission Calculator")
 data_page = st.Page("calculate_data.py", title="Calculate data")
 feedback_page = st.Page("feedback.py", title="Feedback")
 
-st.sidebar.header("Page navigation")
 pg = st.navigation([data_page, feedback_page])
 pg.run()
