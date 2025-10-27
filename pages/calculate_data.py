92d345075fa84f6e6c0223abc4f3e3051b5395b0
gracewildingbs <gwilding@beeverstruthers.co.uk>
2025-10-27 15:23:03 +0000

    change reading excel sheets by position

--- pages/calculate_data.py
+++ pages/calculate_data.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
 
     #st.dataframe(combined_df)
     st.subheader("Non-chargeable expenses", divider=True)
-    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name='Non-Chg')
+    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name=0)
     # print(PE_df)
 
     grouped = PE_df.groupby(PE_df.WIP_Analysis)
@@ -39,7 +39,7 @@ if uploaded_file is not None:
 
     print('-----------------------------------------------------------------------------')
     st.subheader("Chargeable expenses", divider=True)
-    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name='Chg')
+    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name=2)
     # print(PE_df)
 
     grouped = PE_df.groupby(PE_df.WIP_Analysis)
@@ -84,7 +84,7 @@ if uploaded_file is not None:
     st.subheader("Sage expenses", divider=True)
     st.caption("Note: Sage expenses are non-chargeable.")
 
-    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name='Sage NL Downloads 7400 7420')
+    PE_df = pd.read_excel(uploaded_file, index_col=0, sheet_name=3)
     # Uses row from Excel to name the columns
     PE_df.columns = PE_df.iloc[0]
     # Removes row used to name df
@@ -104,9 +104,9 @@ if uploaded_file is not None:
 
     total_sage_flight_costs = (df['Flights'].sum()).round(2)
     st.write('Total flight costs: ', total_sage_flight_costs)
-    st.caption('To calculate mileage from these flights, the necessary rows are below.')
-    flights_df = df.loc[df['Flights'] > 0]
-    st.table(flights_df)
+    # st.caption('To calculate mileage from these flights, the necessary rows are below.')
+    # flights_df = df.loc[df['Flights'] > 0]
+    #st.dataframe(flights_df)
 
     total_sage_hotel_costs = (df['Hotels'].sum()).round(2)
     st.write('Total hotel costs: ', total_sage_hotel_costs)
